0d670619f65b6dc0d20ec3c4ded52c49a44a2c34
sungkuk5420 <sungkuk5420@gmail.com>
2020-07-26 11:46:56 +0900

    Update setting of static files for nginx

--- app.js
+++ app.js
@@ -17,18 +17,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -41,7 +42,7 @@ app.use(function(err, req, res, next) {
 
 var port = '5000';
 
-app.listen(port,function(){
+app.listen(port, function () {
   console.log(`Wetube app listening on port ${port}!`);
 })
 
